e8aad6f70bbd3ef36def71ab56cb66fd1d972ffb
LucaBn <luca.bonotti@virgilio.it>
2023-10-28 01:04:25 +0200

    refactor(popup.js): remove abominations

--- popup.js
+++ popup.js
@@ -1,30 +1,32 @@
 // Initialize button to trigger action
-let daje = document.getElementById("daje");
+const actionButton = document.getElementById("daje");
 
 // When the button is clicked make the magic happen
-daje.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+actionButton.addEventListener("click", async () => {
+  const [activeTab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
 
   chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    target: { tabId: activeTab.id },
+    function: addControlsToVideos,
   });
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
+// The body of this function will be executed as a content script inside the current page
+const addControlsToVideos = () => {
   // Get videos
-  const videos = document.getElementsByTagName("video");
+  const videoElements = document.getElementsByTagName("video");
 
   // Add controls to videos
-  for (let video of videos) {
+  for (let video of videoElements) {
     video.setAttribute("controls", true);
   }
 
   // Add CSS that make controls ALWAYS visible
-  var styles = `video::-webkit-media-controls-panel{display:flex!important;opacity:1!important;z-index:99999!important;pointer-events:all!important;}`;
-  var bonusCSS = document.createElement("style");
-  bonusCSS.innerText = styles;
-  document.head.appendChild(bonusCSS);
-}
+  const cssStyles = `video::-webkit-media-controls-panel{display:flex!important;opacity:1!important;z-index:99999!important;pointer-events:all!important;}`;
+  const customCSS = document.createElement("style");
+  customCSS.innerText = cssStyles;
+  document.head.appendChild(customCSS);
+};
